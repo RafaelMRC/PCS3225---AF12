8fddf9950158c96db73d61fd541d4436da1d1384
Jun-Gondo <guilhermejungondo@gmail.com>
2025-12-09 18:09:11 -0300

    Create memoriaDados.vhd

---
+++ memoriaDados.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity memoriaDados is
+    generic (
+        addressSize : natural := 8;
+        dataSize    : natural := 8
+    );
+    port (
+        clock   : in  bit;
+        we      : in  bit;  -- write enable
+        addr    : in  bit_vector(addressSize-1 downto 0);
+        dIn     : in  bit_vector(dataSize-1 downto 0);
+        dOut    : out bit_vector(dataSize-1 downto 0)
+    );
+end entity memoriaDados;
+
+
+architecture memoriaDados_arch of memoriaDados is
+    type mem_tipo is array (0 to (2**addressSize - 1))
+        of bit_vector(dataSize-1 downto 0);
+
+    signal mem : mem_tipo;
+
+    begin
+    process (clock)
+        if (clock'event and clock = '1') then -- rising_edge
+            if (we = '1') then
+                mem(to_integer(unsigned(addr))) <= dIn;
+            end if;
+        end if;
+    end process;
+
+    dOut <= mem (to_integer(unsigned(addr)));
+end architecture;
